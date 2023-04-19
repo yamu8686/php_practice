e8418f1fbdbf513e689f9f14970ee314d04f6599
yamu8686 <117561782+yamu8686@users.noreply.github.com>
2023-04-19 09:28:04 +0000

    代入

---
+++ dainyu.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    $x = 5;
+    $y = 2;
+    $a = 8;
+    $b = 4;
+
+  ?>
+
+  <!-- この下で$xの計算をしていきましょう -->
+  <?php
+    $x = $x + 10;
+    echo $x;
+
+  ?>
+
+  <br>
+
+  <!-- この下で$yの計算をしていきましょう -->
+  <?php
+    $y = $y * 5;
+    echo $y;
+
+  ?>
+
+  <br>
+
+  <!-- この下で$aの計算をしていきましょう -->
+  <?php
+    $a = $a + 1;
+    echo $a;
+  ?>
+
+  <br>
+
+  <!-- この下で$bの計算をしていきましょう -->
+  <?php
+    $b = $b - 1;
+    echo $b;
+
+  ?>
+
+</body>
+</html>
